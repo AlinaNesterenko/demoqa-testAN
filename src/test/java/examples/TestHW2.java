037839d36a693611b5dd9ca60d1858d6a6ac471f
Alina Nesterenko <alina.zaitsava@minsk.leveris.com>
2023-09-14 21:56:15 +0300

    HW 2 - finish variant

--- src/test/java/examples/TestHW2.java
+++ src/test/java/examples/TestHW2.java
@@ -1,11 +1,8 @@
 package examples;
 
-//import java.lang.module.Configuration;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Wait;
 
 import com.codeborne.selenide.Configuration;
 
@@ -14,9 +11,8 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.files.DownloadActions.click;
 
-public class testhw2 {
+public class TestHW2 {
 
   @BeforeAll
   static void SetUp() {
@@ -24,14 +20,9 @@ public class testhw2 {
     Configuration.browserSize="1024x800";
 
   }
-  //@Test
-  //void firstTest() {
-  //  System.out.println("_______firstTest()");
-//
- // }
 
   @Test
-  void FillFormTest() throws InterruptedException {
+  void fillFormTest()  {
     open("https://demoqa.com/automation-practice-form");
 
 
@@ -42,28 +33,19 @@ public class testhw2 {
     $("#firstName").setValue("Alina");
     $("#lastName").setValue("N");
     $("#userEmail").setValue("Alina@mail.com");
-
-
-   // Object gender;
-  //  $("#gender-radio-2").click ();
-    $(byText("Female")).click();
+    $("#genterWrapper").$(byText("Female")).click();
     $("#userNumber").setValue("1234567890");
     $("#dateOfBirthInput").click();
     $(".react-datepicker__month-dropdown-container").$(byText("September")).click();
     $(".react-datepicker__year-select").selectOption("1990");
     $(".react-datepicker__day--002").click();
-   // $("#subjectsInput").setValue("Test");
-   //$(byText("Reading")).click();
     $("#subjectsInput").setValue("Chemistry").pressEnter();
-    $(byText("Reading")).click();
+    $("#hobbiesWrapper").$(byText("Reading")).click();
+    $("#uploadPicture").uploadFromClasspath("1.png");
     $("#currentAddress").setValue("Street, appartment, home, town, district, region, country");
-
-
-
-    //  $(byText("Select state")).click();
     $("#state").click();
     $("#stateCity-wrapper").$(byText("NCR")).click();
-   $("#city").click();
+    $("#city").click();
     $("#stateCity-wrapper").$(byText("Delhi")).click();
     $("#submit").click();
 
@@ -77,20 +59,10 @@ public class testhw2 {
     $(".modal-content").shouldHave(text("1234567890"));
     $(".modal-content").shouldHave(text("02 September,1990"));
     $(".modal-content").shouldHave(text("Chemistry"));
- //   $(".modal-content").shouldHave(text("hw2-2.png"));
+    $(".modal-content").shouldHave(text("1.png"));
     $(".modal-content").shouldHave(text("Street, appartment, home, town, district, region, country"));
     $(".modal-content").shouldHave(text("NCR Delhi"));
 
-
-
-
-//    $("#submit").click();
-// read values
- /*   $("#output #name").shouldHave(text("Alina"));
-    $("#output #email").shouldHave(text("alina@mail.com"));
-    $("#output #currentAddress").shouldHave(text("Street"));
-    $("#output #permanentAddress").shouldHave(text("town"));
-  */
   }
 
 
