9fd6b90d91e680289a00d59795cdd071219e7eec
Alina Nesterenko <alina.zaitsava@minsk.leveris.com>
2023-11-10 22:29:42 +0300

    HW - 13 jenkins1 - version 1

--- src/test/java/examples/TestBase.java
+++ src/test/java/examples/TestBase.java
@@ -1,16 +1,41 @@
 package examples;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+
+import examples.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 
 public class TestBase {
   @BeforeAll
   static void setUp() {
     Configuration.baseUrl = "https://demoqa.com";
-    Configuration.browserSize="1024x800";
+    Configuration.browserSize = "1024x800";
     Configuration.pageLoadStrategy = "eager";
     Configuration.timeout = 10000;
+    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("enableVNC", true);
+    capabilities.setCapability("enableVideo", true);
+    Configuration.browserCapabilities = capabilities;
 
+  }
+
+  @BeforeEach
+  void addListener() {
+    SelenideLogger.addListener("allure", new AllureSelenide());
+  }
+
+  @AfterEach
+  void addAttachments(){
+    Attach.screenshotAs("Last state");
+    Attach.pageSource();
+    Attach.browserConsoleLogs();
+    Attach.addVideo();
   }
 }
---
+++ src/test/java/examples/TestHW13Jenkins.java
@@ -0,0 +1,106 @@
+package examples;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
+import pages.RegistrationPage;
+import pages.components.ResultModal;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
+public class TestHW13Jenkins extends TestBase {
+
+
+  RegistrationPage registrationPage = new RegistrationPage();
+  TestData testData = new TestData();
+  ResultModal resultForm = new ResultModal();
+
+  @Test
+  @Tag("jenkinsTest")
+  @DisplayName("Successfull Registration")
+  void fillFormTest() {
+
+
+    step("Открывыем страницу регистрации", () -> {
+      registrationPage.openPage();
+    });
+    step("Добавляем Имя", () -> {
+      registrationPage.setFirstName(testData.firstName);
+    });
+    step("Добавляем Фамилию", () -> {
+      registrationPage.setLastName(testData.lastName);
+    });
+    step("Добавляем Email", () -> {
+      registrationPage.setUserEmail(testData.userEmail);
+    });
+    step("Устанавливаем пол", () -> {
+      registrationPage.setGender(testData.userGender);
+    });
+    step("Добавляем Номер", () -> {
+      registrationPage.setUserNumber(testData.userNumber);
+    });
+    step("Добавляем Дату рождения", () -> {
+      registrationPage.setDateOfBirth(testData.userDayB, testData.userMonthB, testData.userYearB);
+    });
+    step("Добавляем Предмет", () -> {
+      registrationPage.setSubjects(testData.userSubj);
+    });
+    step("Добавляем Хобби", () -> {
+      registrationPage.setHobbies(testData.userHobby);
+    });
+    step("Загружаем фото", () -> {
+      registrationPage.uploadPicture(testData.userImg);
+    });
+    step("Добавляем Адрес", () -> {
+      registrationPage.setCurrentAddress(testData.userAddress);
+    });
+    step("Добавляем Штат и Город", () -> {
+      registrationPage.setState(testData.userState).setCity(testData.userCity);
+    });
+    step("Подтверждаем регистрацию", () -> {
+      registrationPage.submitAction();
+    });
+
+    step("Проверяем Имя и Фамилию студента", () -> {
+      resultForm.checkResultText("Student Name", testData.firstName + " " + testData.lastName);
+    });
+
+    step("Проверяем Email", () -> {
+      resultForm.checkResultText("Student Email", testData.userEmail);
+    });
+    step("Проверяем пол", () -> {
+      resultForm.checkResultText("Gender", testData.userGender);
+    });
+    step("Проверяем Номер", () -> {
+      resultForm.checkResultText("Mobile", testData.userNumber);
+    });
+    step("Проверяем Дату рождения", () -> {
+      resultForm.checkResultText("Date of Birth", testData.userDayB + " " + testData.userMonthB + "," + testData.userYearB);
+    });
+    step("Проверяем Предмет", () -> {
+      resultForm.checkResultText("Subjects", testData.userSubj);
+    });
+    step("Проверяем Хобби", () -> {
+      resultForm.checkResultText("Hobbies", testData.userHobby);
+    });
+    step("ЗПроверяем фото", () -> {
+      resultForm.checkResultText("Picture", testData.userImg);
+    });
+    step("Проверяем Адрес", () -> {
+      resultForm.checkResultText("Address", testData.userAddress);
+    });
+    step("Проверяем Штат и Город", () -> {
+      resultForm.checkResultText("State and City", testData.userState + " " + testData.userCity);
+    });
+
+  }
+}
